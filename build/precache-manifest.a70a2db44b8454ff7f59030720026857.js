22f4ed831a766a0b84bad5868997dd5a47c6b13e
iamraghavan <developer.raghavan@gmail.com>
2023-06-08 18:50:11 +0530

    Hello Raghavan

---
+++ build/precache-manifest.a70a2db44b8454ff7f59030720026857.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8e30557197b5b67df46",
+    "url": "/static/css/main.4a47225a.chunk.css"
+  },
+  {
+    "revision": "b8e30557197b5b67df46",
+    "url": "/static/js/main.b8e30557.chunk.js"
+  },
+  {
+    "revision": "11419f9ab822d4f34c57",
+    "url": "/static/js/1.11419f9a.chunk.js"
+  },
+  {
+    "revision": "0e486b45267265568ed7",
+    "url": "/static/js/2.0e486b45.chunk.js"
+  },
+  {
+    "revision": "9c47fb1e42a298e0465e",
+    "url": "/static/js/runtime~main.9c47fb1e.js"
+  },
+  {
+    "revision": "71518c98b01c056991564c8db9d25964",
+    "url": "/index.html"
+  }
+];
